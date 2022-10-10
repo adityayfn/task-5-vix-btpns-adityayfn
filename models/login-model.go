56016b1b483f2907a9f4c97e1a110fd55b3c3e7b
aditya yudha <69260395+adityayfn@users.noreply.github.com>
2022-10-11 01:08:38 +0700

    Delete models directory

--- models/login-model.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type LoginModel struct {
-	Email    string `json:"email" form:"email" binding:"required" validate:"email"`
-	Password string `json:"password" form:"password" binding:"required" validate:"min:6"`
-}
