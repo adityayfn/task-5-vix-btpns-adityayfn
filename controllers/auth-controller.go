95ea9fabbb60824026dab8b1928d63bd194da8cf
adityayfn <adhitya.firstian@gmail.com>
2022-10-11 00:27:51 +0700

    update auth controller

--- controllers/auth-controller.go
+++ controllers/auth-controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import "github.com/gin-gonic/gin"
 
+
 type AuthController interface{
 	Login(ctx *gin.Context)
 	Register(ctx *gin.Context)
@@ -15,13 +16,13 @@ func NewAuthController() AuthController{
 	return &authController{}
 }
 func(c *authController)Login(ctx *gin.Context){
-	ctx.JSON(http.StatusOk,gin H{
-		"message" : "Hello Login"
+	ctx.JSON(http.StatusOk,gin.H{
+		"message" : "Hello Login",
 	})
 }
 
 func(c *authController)Register(ctx *gin.Context){
-	ctx.JSON(http.StatusOk,gin H{
-		"message" : "Hello Register"
+	ctx.JSON(http.StatusOk,gin.H{
+		"message" : "Hello Register",
 	})
 }
